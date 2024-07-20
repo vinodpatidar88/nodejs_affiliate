3ba6d638a9a278e41938c3ee9049846cc1d488a6
Akhand Pratap Rai <akhandpratap892@gmail.com>
2024-07-20 15:24:55 +0530

    added scrap for mytntra

--- app.js
+++ app.js
@@ -19,6 +19,41 @@ const scrapeAmazonProduct = async (url) => {
     return productData;
 };
 
+const scrapeMyntraProduct = async (url) => {
+    let browser;
+    try {
+        browser = await puppeteer.launch({
+            headless: true,
+            args: [
+                '--disable-web-security',
+                '--disable-features=IsolateOrigins,site-per-process'
+            ]
+        });
+        const page = await browser.newPage();
+
+        await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36');
+
+        console.log(`Navigating to URL: ${url}`);
+        await page.goto(url, { waitUntil: 'networkidle2', timeout: 60000 });
+
+        const productData = await page.evaluate(() => {
+            const title = document.querySelector('.pdp-title')?.innerText.trim();
+
+            return { title };
+        });
+
+        console.log(`Scraped data: ${JSON.stringify(productData)}`);
+        return productData;
+    } catch (error) {
+        console.error(`Error during Puppeteer operation: ${error.message}`);
+        throw error;
+    } finally {
+        if (browser) {
+            await browser.close();
+        }
+    }
+};
+
 app.get('/scraper', async (req, res) => {
     const url = req.query.url;
 
@@ -27,7 +62,7 @@ app.get('/scraper', async (req, res) => {
         return res.status(400).send('URL is required');
     }
     try {
-        const data = await scrapeAmazonProduct(url);
+        const data = await scrapeMyntraProduct(url);
         // console.log(data);
         res.status(200).json(data);
     } catch (error) {
